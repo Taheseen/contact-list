35b7ee246d0f53bef28095c6da1b4d795917626f
Tahesin <tahesinara.tahesinara@nl.abnamro.com>
2019-08-26 17:15:14 +0200

    updated

--- src/mixins/service.js
+++ src/mixins/service.js
@@ -14,16 +14,3 @@ export default {
     return self.contactList
   }
 }
-
-//   var self = this
-//   self.contactList = ''
-//   this.$http
-//     .get('https://randomuser.me/api/?results=50')
-//     .then(response => {
-//       self.contactList = response.data
-//     })
-//   console.log(self.contactList, 'servie')
-//   return self.contactList
-//     }
-//   }
-// }
